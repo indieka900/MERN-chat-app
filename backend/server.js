a32fd65d8bb656bc2d316f7f07ad05da8ce7dcfc
Joseph <indiekaj@gmail.com>
2024-08-25 01:34:38 +0300

    socket io, setting online status

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,10 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import userRoutes from './routes/user.routes.js';
 import connectMongoDB from "./db/connect.js";
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 8000;
 
 // app.use(cors({
@@ -33,7 +33,7 @@ app.use("/api/users", userRoutes);
 const startServer = async () => {
     try {
         await connectMongoDB();
-        app.listen(PORT, () => {
+        server.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
     } catch (error) {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,16 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 //import StepOne from "./components/test/Test.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextProvider>
-        <App />
-        {/* <StepOne /> */}
+        <SocketContextProvider>
+          <App />
+        </SocketContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
