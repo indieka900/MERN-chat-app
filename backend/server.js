16c4299e39c5a66dc31663c3b2691cab5fe53f54
Joseph <indiekaj@gmail.com>
2024-08-25 02:33:47 +0300

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from 'cors';
+import cors from "cors";
 
-import authRoutes from './routes/auth.routes.js';
-import messageRoutes from './routes/message.routes.js';
-import userRoutes from './routes/user.routes.js';
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectMongoDB from "./db/connect.js";
 import { app, server } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -24,22 +27,23 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
-// app.get("/", (req, res) => {
-//     res.status(200).send({"message": "Hello world"});
-// });
 
 const startServer = async () => {
-    try {
-        await connectMongoDB();
-        server.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    } catch (error) {
-        console.error("Failed to connect to MongoDB", error);
-        process.exit(1); // Exit the process with an error code
-    }
+  try {
+    await connectMongoDB();
+    server.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+    process.exit(1); // Exit the process with an error code
+  }
 };
 
 startServer();
